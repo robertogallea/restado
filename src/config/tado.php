c3bb72a79a0050c52d1ff1085ace011d9f1499a5
Pim Hofman <github@pimhofman.nl>
2021-11-03 19:56:18 +0100

    Home ID no longer necessary in config

--- src/config/tado.php
+++ src/config/tado.php
@@ -4,7 +4,6 @@ return [
 
     'username' => env('TADO_USER', 'username'),
     'password' => env('TADO_PASS', '********'),
-    'home_id'  => env('TADO_HOME_ID', '123456'),
 
     'clientId'      => env('TADO_CLIENT_ID','public-api-preview'),
     'clientSecret'  => env('TADO_SECRET','4HJGRffVR8xb3XdEUQpjgZ1VplJi6Xgw'),
